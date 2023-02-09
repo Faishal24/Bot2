b5c4b7c5361d25163f0f86fc2a16ca802fe41044
Faishal <ishal.2401@gmail.com>
2023-02-09 21:07:54 +0700

    Initial commit

---
+++ plugins/menu-awal.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+ await conn.sendButton(m.chat, `wm + '\n\n' + botdate, giflogo, [['𝘼𝙇𝙇 𝙈𝙀𝙉𝙐','.? all'],['𝙇𝙄𝙎𝙏 𝙈𝙀𝙉𝙐','.siuuu']], m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: bottime,
+                        body: 'Total Cintaku Padamu',          previewType: 0,
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: sig
+                      }}
+})
+}
+
+
+handler.help = ['totalfitur']
+handler.tags = ['info']
+handler.command = /^(menu|help|co)$/i
+export default handler
